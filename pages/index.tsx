3e05250dee1826c28218d7d9a7ed0a251f2ff94a
RiciTrix <richardgroenewald5@gmail.com>
2026-07-04 20:37:32 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,20 +12,7 @@ type Props = {
 const IndexPage: NextPage<Props> = ({ countries }) => {
   return (
     <>
-      <SEOHead />
-      <div className="m-16 mx-auto container">
-        <Countries items={countries} />
-      </div>
-      <hr />
-      <Image
-        className="h-8 mx-auto m-12 md:mt-16"
-        src="//data.commercelayer.app/assets/logos/full-logo/black/commercelayer_full_logo_black.svg"
-        alt="Commerce Layer Logo"
-        loading="eager"
-        width={200}
-        height={50}
-      />
-      <br />
+	<p>Test</p>
     </>
   );
 };
